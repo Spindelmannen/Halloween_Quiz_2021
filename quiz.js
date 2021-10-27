b6b9d8e8c4dc4344e72e87435a1aa357628df66e
Spindelmannen <jonteadolfsson1995@gmail.com>
2021-10-27 23:08:16 +0200

    updated and added code in javascript file

--- quiz.js
+++ quiz.js
@@ -18,4 +18,100 @@ let mainDiv = document.querySelector("#content");
     mainDiv.style.color = "black";
     switchMode.innerHTML ="<strong>Give me darkness!</strong>";
 }
+    // label.forEach((text) => { //En forEach loop för alla label texter.
+    //     if (body.style.background === "darkslategray") { //om bakgrunden är darkslategray...
+    //     text.style.color = "white"; //..blir alla labels vita
+    //     } else {
+    //     text.style.color = "black"; //annars är labels svarta.
+    //     }
+    // });
+    //     input.forEach((radiotext) => { 
+    //         if (body.style.background !== "lightskyblue") { 
+    //         radiotext.style.color = "white";
+    //         console.log(input);
+    //         } else {
+    //         radiotext.style.color = "white"; 
+    //         }
+    //     });
+    });
+    
+
+
+//RESTART KNAPP
+let startaOm = document.querySelector("#restart"); 
+startaOm.addEventListener("click",() => { //en knapp som refreshar sidan.
+    window.location.reload();
+    valdaSvar.forEach((item) => {
+        valdaSvar.remove();
+    })
+});
+
+//let checkBox = document.getElementsByClassName("checkbox");
+
+let resultatBox = document.createElement("div")
+let resultatText = document.createElement("p")
+let answers = document.querySelectorAll(".question"); //hämtar rätta radiosvar
+let resultat = document.querySelector("#doneButton") // resultat knapp
+
+//CHECK ANSWERS KNAPP
+resultat.addEventListener("click", () => {
+resultatText.innerHTML = "";
+let poäng =0;
+
+    //en forEach loop genom alla rätta radiosvar och lägger 1 poäng på varje rätta radiosvar
+    answers.forEach((item) => {
+        if (item.checked === true){
+            poäng++;
+        }
+    });
+
+    //Checkboxes
+    if (document.querySelector("#q8a1").checked === true && document.querySelector("#q8a4").checked === true 
+    && !document.querySelector("#q8a3").checked === true && !document.querySelector("#q8a2").checked === true) {
+        poäng++;
+    } else {
+        poäng+0;
+    };
+
+resultatText.innerHTML = "<strong>You Scored: "+poäng +"/10 Points!</strong>";
+if (poäng >=8){
+    resultatText.style.color = "green";
+} else if (poäng >=5) {
+    resultatText.style.color = "orange"; 
+} else {
+    resultatText.style.color = "red";
+}
+
+//Gammal lösning som ersattes av forEach loop.
+// if (document.querySelector("#q1a2").checked === true){
+//     poäng++; 
+// }
+// if (document.querySelector("#q2a1").checked === true){
+//     poäng++; 
+// }
+// if (document.querySelector("#q3a1").checked === true){
+//     poäng++; 
+// }
+// if (document.querySelector("#q4a3").checked === true){
+//     poäng++; 
+// }
+// if (document.querySelector("#q5a1").checked === true){
+//     poäng++; 
+// }
+// if (document.querySelector("#q6a3").checked === true){
+//     poäng++; 
+// }
+// if (document.querySelector("#q7a2").checked === true){
+//     poäng++; 
+// }
+// if (document.querySelector("#q9a2").checked === true){
+//     poäng++; 
+// }
+// if (document.querySelector("#q10a3").checked === true){
+//     poäng++; 
+// }
+    console.log(poäng);
+resultat.appendChild(resultatBox);
+resultatBox.appendChild(resultatText);
+});
 
